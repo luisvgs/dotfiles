d84ca9af7c75fb5b4f9d99c566cb860e2051faee
bluretrece <luisvegasmor@gmail.com>
2022-02-07 20:18:36 -0400

    New scheme

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -3,10 +3,10 @@ vim.g.tokyonight_italic_comments = 0
 vim.g.tokyonight_italic_keywords = 0
 vim.g.tokyonight_style = "night"
 -- vim.cmd[[colorscheme jellybeans-nvim]]
--- vim.cmd[[colorscheme doom-one]]
-vim.cmd[[colorscheme tokyonight]]
+vim.cmd[[colorscheme doom-one]]
+-- vim.cmd[[colorscheme tokyonight]]
 -- vim.cmd[[colorscheme nimda]]
-require('onedark').setup {
-    style = 'warmer'
-}
+-- require('onedark').setup {
+--     style = 'warmer'
+-- }
 -- require('onedark').load()
