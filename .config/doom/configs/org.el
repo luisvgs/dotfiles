7e031f28b52c926fa273ff2789d7449d227da422
Luis Vegas <luisvegasmor@gmail.com>
2023-10-19 15:51:45 -0400

    Org-agenda config for UI enhancement

--- .config/doom/configs/org.el
+++ .config/doom/configs/org.el
@@ -1,8 +1,34 @@
-(after! org-mode
-  org-directory "~/org/"
-  todo-file "~/org/todo.org"
-  org-agenda-files '("~/org")
-  (setq org-bullets-bullet-list '("#")))
+(after! org
+  org-directory "~/org/")
+
+(setq
+ org-fancy-priorities-list '("[A]" "[B]" "[C]")
+ org-priority-faces
+ '((?A :foreground "#ff6c6b" :weight bold)
+   (?B :foreground "#98be65" :weight bold)
+   (?C :foreground "#c678dd" :weight bold))
+ org-agenda-block-separator 8411)
+
+(setq org-capture-templates
+      '(("t" "todo" entry (file+headline "~/org/todo.org" "Tasks")
+         "* TODO [#A] %?\nSCHEDULED: %(org-insert-time-stamp (org-read-date nil t \"+0d\"))\n")))
+
+(setq org-agenda-custom-commands
+      '(("v" "Custom agenda view"
+         ((tags "PRIORITY=\"A\""
+                ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                 (org-agenda-overriding-header "High-priority unfinished tasks:")))
+          (tags "PRIORITY=\"B\""
+                ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                 (org-agenda-overriding-header "Medium-priority unfinished tasks:")))
+          (tags "PRIORITY=\"C\""
+                ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                 (org-agenda-overriding-header "Low-priority unfinished tasks:")))
+          (agenda "" ((org-agenda-ndays 1)))
+          (alltodo ""
+                   ((org-agenda-skip-function '(org-agenda-skip-if nil '(scheduled deadline)))
+                    (org-agenda-overriding-header "ALL normal priority tasks:"))))
+         ((org-agenda-compact-blocks nil)))))
 
 (use-package! org-superstar
   :hook (org-mode . org-superstar-mode))
