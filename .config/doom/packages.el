855cb014c9aefb3ee2c55de3af084845d0c830c3
Luis Vegas <luisvegasmor@gmail.com>
2023-10-19 21:41:26 -0400

    renamed files following emacs '+' file prefix

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -1,4 +1,4 @@
 (package! latex-preview-pane)
-(package! jbeans-theme)
+;; (package! jbeans-theme)
 (package! ef-themes)
 (disable-packages! jbeans-theme nodejs-repl coffee-mode skewer-mode all-the-icons)
