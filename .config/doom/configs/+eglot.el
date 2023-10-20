6d4efba2cc51f466dcfa0e95a5a3e55ba6f57aa5
Luis Vegas <luisvegasmor@gmail.com>
2023-10-20 19:49:51 -0400

    lsp: added scala mode

--- .config/doom/configs/+eglot.el
+++ .config/doom/configs/+eglot.el
@@ -1,6 +1,7 @@
 (use-package! eglot
+  :defer t
   :config
   (setq eglot-ignored-server-capabilities '(:hoverProvider :documentLinkProvider :inlayHintProvider :documentOnTypeFormattingProvider))
   (add-to-list 'eglot-server-programs '(rustic-mode "rust-analyzer"))
   :hook
-  ((rjsx-mode rustic-mode typescript-mode js2-mode) . eglot-ensure))
+  ((rjsx-mode rustic-mode typescript-mode js2-mode scala-mode) . eglot-ensure))
