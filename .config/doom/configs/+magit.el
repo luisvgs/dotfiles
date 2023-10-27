9ccd8a8945bbab806b2d5fba5d87c250f965806d
Luis Vegas <luisvegasmor@gmail.com>
2023-10-27 14:03:26 -0400

    magit tweaks

--- .config/doom/configs/+magit.el
+++ .config/doom/configs/+magit.el
@@ -1,2 +1,6 @@
-(use-package! magit
-  :commands magit-status)
+;; (use-package! magit
+;;   :config
+;;   (setq magit-branch-read-upstream-first 'fallback)
+;;   :commands magit-status)
+(after! magit
+  (setq magit-branch-read-upstream-first 'fallback))
