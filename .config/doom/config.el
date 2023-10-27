a56b68bb5a5c73f53b121b4341ef9d651cf8c72e
Luis Vegas <luisvegasmor@gmail.com>
2023-10-27 14:03:37 -0400

    modeline and mini buffer UI tweaks

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -7,11 +7,12 @@
       ;; doom-theme 'doom-solarized-dark-high-contrast
       ;; doom-theme 'modus-vivendi-deuteranopia
       ;; doom-theme 'doom-one
+      doom-theme 'doom-vibrant
       ;; doom-theme 'doom-zenburn
       ;; doom-theme 'doom-tomorrow-night
       ;; doom-theme 'ef-winter
       ;; doom-theme 'doom-wilmersdorf
-      doom-theme 'doom-flatwhite
+      ;; doom-theme 'doom-flatwhite
       use-package-compute-statistics t
       auto-save-default t
       so-long-minor-mode t
@@ -20,14 +21,35 @@
       initial-major-mode (quote fundamental-mode)
       display-line-numbers-type 'relative)
 
+;; (after! persp-mode
+;;   ;; alternative, non-fancy version which only centers the output of +workspace--tabline
+;;   (defun workspaces-formatted ()
+;;     (+doom-dashboard--center (frame-width) (+workspace--tabline)))
+
+;;   (defun hy/invisible-current-workspace ()
+;;     "The tab bar doesn't update when only faces change (i.e. the
+;; current workspace), so we invisibly print the current workspace
+;; name as well to trigger updates"
+;;     (propertize (safe-persp-name (get-current-persp)) 'invisible t))
+
+;;   (customize-set-variable 'tab-bar-format '(workspaces-formatted tab-bar-format-align-right hy/invisible-current-workspace))
+
+;;   ;; don't show current workspaces when we switch, since we always see them
+;;   (advice-add #'+workspace/display :override #'ignore)
+;;   ;; same for renaming and deleting (and saving, but oh well)
+;;   (advice-add #'+workspace-message :override #'ignore))
+
+;; ;; need to run this later for it to not break frame size for some reason
+;; (run-at-time nil nil (cmd! (tab-bar-mode +1)))
+
 (add-hook 'window-setup-hook #'toggle-frame-fullscreen)
+
 (unless (equal "Battery status not available"
                (battery))
   (display-battery-mode 1))
 
 (advice-add #'add-node-modules-path :override #'ignore)
 
-(global-set-key (kbd "C-x C-b") 'ibuffer)
 (setq-default
  delete-by-moving-to-trash t
  window-combination-resize t
@@ -56,6 +78,7 @@
 (load! "configs/+org")
 (load! "configs/+eglot")
 (load! "configs/+dashboard")
+;; (load! "configs/+treemacs")
 (load! "configs/+utility")
 (load! "configs/+keybindings")
 (load! "configs/+latex")
