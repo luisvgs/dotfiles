b29e7a569c58aefb4ce0d224b9651ed4eed43b43
Luis Vegas <luisvegasmor@gmail.com>
2023-11-06 17:00:12 -0500

    tweaked the scala helper a bit to support newest version

--- .config/zsh/scripts/scala-helper/scala-helper.zsh
+++ .config/zsh/scripts/scala-helper/scala-helper.zsh
@@ -5,6 +5,8 @@ function scala-helper() {
     .idea/
     .metals/
     .bloop/
+    .ammonite/
+    metals.sbt
     .scala-build/
     target/' > .gitignore
     touch .scalafmt.conf
@@ -13,8 +15,11 @@ function scala-helper() {
     mkdir -p src/{main,test}/{scala,resources} project
     touch src/main/scala/Main.scala
     echo '@main def hello: Unit = println("Hello world!")' > src/main/scala/Main.scala
-    echo 'sbt.version=1.6.2' > ./project/build.properties
-    echo 'name := "NewProject"
-    version := "1.9.7"
-    scalaVersion := "3.3.1"' > build.sbt
+    echo 'sbt.version=1.9.7' > ./project/build.properties
+    echo 'ThisBuild / name := "NewProject"
+    ThisBuild / version := "1"
+    ThisBuild / scalaVersion := "3.3.1"
+    Global / parallelExecution := false
+    lazy val root = (project in file("."))
+    .settings(name := "NewProject")' > build.sbt
 }
