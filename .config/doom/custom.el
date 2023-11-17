11fee3920413e8dbd901267aac35764a49911ec7
Luis Vegas <luisvegasmor@gmail.com>
2023-11-17 18:40:46 -0500

    custom emacs tweaks

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -3,11 +3,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(doom-modeline-buffer-modified ((t (:foreground "orange")))))
+ '(doom-modeline-buffer-modified ((t (:foreground "orange"))))
+ '(org-document-title ((t (:height 1.1 :underline nil :foreground "#8B8B8B"))))
+ '(org-level-1 ((t (:inherit outline-1 :height 1.45 :background "#222222"))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.1))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.0)))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
- '(org-agenda-files '("~/org/todo.org")))
+ '(org-agenda-files '("~/org/todo.org"))
+ '(package-selected-packages '(flymake)))
