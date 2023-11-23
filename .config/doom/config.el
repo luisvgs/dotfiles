ba8682e0156c5f13789e0ed26983b0c132a0c7dc
Luis Vegas <luisvegasmor@gmail.com>
2023-11-22 21:22:27 -0500

    lexical-binding directives (para que es?)

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -1,3 +1,4 @@
+;; -*- lexical-binding: t -*-
 (use-package! doom)
 (setq
  user-full-name "Luis Vegas"
--- .config/doom/init.el
+++ .config/doom/init.el
@@ -1,3 +1,4 @@
+;; -*- lexical-binding: t -*-
 (doom! :input
        :completion
        ( ivy +icons)
