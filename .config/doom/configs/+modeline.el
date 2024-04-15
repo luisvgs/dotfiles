bf29562880bdbfba7f0c35bc8246f6d2b105611d
Luis Vegas <luisvegasmor@gmail.com>
2024-04-15 09:32:02 -0400

    modeline tweaks

--- .config/doom/configs/+modeline.el
+++ .config/doom/configs/+modeline.el
@@ -1,12 +1,9 @@
 (use-package! doom-modeline
   :config
   (setq doom-modeline-height 30
-        doom-modeline-battery t
-        doom-themes-padded-modeline t
-        doom-flatwhite-brighter-modeline nil
         doom-modeline-unicode-fallback t
         doom-modeline-icon t
-        doom-plain-brighter-modeline nil
+        doom-plain-brighter-modeline t
         doom-modeline-major-mode-icon t
         doom-modeline-major-mode-color-icon t
         doom-modeline-buffer-file-name-style 'truncate-with-project
@@ -15,12 +12,12 @@
         doom-modeline-buffer-modification-icon t
         doom-modeline-buffer-name t
         doom-modeline-modal t
-        doom-modeline-modal-modern-icon nil
+        doom-modeline-modal-modern-icon t
         doom-modeline-modal-icon t
         doom-modeline-lsp-icon t
         doom-modeline-lsp nil
         doom-modeline-modal nil
-        doom-modeline-persp-name t)
+        doom-modeline-persp-name nil)
   (custom-set-faces!
     '(doom-modeline-buffer-modified :foreground "orange")))
 
